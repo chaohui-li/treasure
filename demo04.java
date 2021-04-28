d993c71d59092b61ac344efd7fff2591eb610c7c
king-0704 <74943646+king-0704@users.noreply.github.com>
2021-04-29 07:49:25 +0800

    Add files via upload

---
+++ demo04.java
@@ -0,0 +1,13 @@
+package xc.dq;
+public class demo04 {
+    public static void main(String[] args){
+        int num=2;
+        int sum=0;
+        while (num<=100){
+            sum+=num;
+            System.out.println("num:"+num+"\tsum:"+sum);
+            num+=2;
+        }
+
+    }
+}
