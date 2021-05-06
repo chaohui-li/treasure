956992d5fcd61c72d7ced8db46f3c423197427e7
king-0704 <74943646+king-0704@users.noreply.github.com>
2021-05-06 08:31:38 +0800

    Add files via upload

---
+++ demo06.java
@@ -0,0 +1,18 @@
+package xc.dq;
+
+import java.util.Scanner;
+
+public class demo06 {
+    public static void main(String[] args){
+        int sum=0;
+        for (int i =0;i<100;i++){
+            if(i%3!=0){
+                sum+=i;
+            }
+
+        }
+
+        System.out.println("不能被3整除的数之和："+sum);
+
+    }
+}
