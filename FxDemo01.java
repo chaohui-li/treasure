1a410d241b679b5b1168943bc37c8e926a6e6f8d
king-0704 <74943646+king-0704@users.noreply.github.com>
2021-05-27 07:50:22 +0800

    Add files via upload

--- FxDemo01.java
+++ FxDemo01.java
@@ -1,18 +1,14 @@
-package 泛型;
-
-import java.util.ArrayList;
+package 泛型02;
 
 public class FxDemo01 {
     public static void main(String[]args){
-        ArrayList list=new ArrayList();
-        list.add(11);
-        list.add("1234");
-        list.add(true);
-        list.add(1221,123);
-        System.out.println(list);
+        Integer[]a={12,23,234,45,42};
+        System.out.println(Tools.getMiddle(a));
 
-        ArrayList<String>list1=new ArrayList<>();
-        list1.add("fdcdf");
-        // list.add(121);泛型做类型的约束，使集合中存放相同的数据类型
+        String[] str ={"add","sdafq","awdqw"};
+        System.out.println(Tools.getMiddle(str));
+
+        Boolean[]b={true,false,true,false};
+        System.out.println(Tools.getMiddle(b));
     }
 }
